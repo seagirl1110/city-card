2588772c695e2ce0e576c522ca8a09b863d7ccaf
Marina Elizarova <seagirl1110@gmail.com>
2024-08-21 19:59:15 +0300

    feat: add CitySelector

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import { useState } from 'react';
 import './App.css';
+import CitySelector from './components/citySelector';
 
 function App() {
-  return <div className="App"></div>;
+  const [card, setCard] = useState({
+    name: 'Осака',
+    description:
+      'Город в центральной части острова Хонсю, известен своими современными достопримечательностями и активной ночной жизнью.',
+    imageUrl:
+      'https://sportishka.com/uploads/posts/2022-04/1650631594_29-sportishka-com-p-osaka-gorod-v-yaponii-krasivo-foto-30.jpg',
+    facts: [
+      'Осака известна своим замком, который играл ключевую роль в объединении Японии в XVI веке.',
+      'Город является кулинарной столицей Японии.',
+    ],
+  });
+
+  const setSelectedCard = (selectedCard) => {
+    setCard(selectedCard);
+  };
+
+  return (
+    <div className="App">
+      <CitySelector selectedCard={card} setSelectedCard={setSelectedCard} />
+    </div>
+  );
 }
 
 export default App;
